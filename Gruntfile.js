755a3ff0015f19e2e52960428c4cdc7d2c66de6e
Steen Lehmann <steen.lehmann@gmail.com>
2015-09-08 09:23:29 +0200

    Grunt task for watching files

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    shell: {
+      deploy: {
+        command: "git push"
+      },
+      asciidoc: {
+        command: "asciidoc -b html5 -o public/index.html -a linkcss -a stylesheet=tufte.css st33n.adoc"
+      }
+    },
+    watch: {
+      asciidoc: {
+        files: [ '*.conf', '*.adoc' ],
+        tasks: ['shell:asciidoc']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('deploy', ['shell:deploy']);
+  grunt.registerTask('default', ['shell:asciidoc', 'watch']);
+
+};
+
--- go.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-asciidoc -b html5 -o public/index.html -a linkcss -a stylesheet=tufte.css st33n.adoc 
-
